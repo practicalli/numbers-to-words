f6c6ecf3b036601677a6130d1562635cf4e1111d
John Stevenson <john@jr0cket.co.uk>
2019-04-09 14:30:38 +0100

    Test: high level sample data tests
    Commented out lower level tests and added high level tests more relevant to
    testing the namespace

--- test/word_conversion/core_test.clj
+++ test/word_conversion/core_test.clj
@@ -9,60 +9,81 @@
   "Test the conversion of numerical whole numbers to
    strings containing British English words as a sentence"
 
-  (testing "Sample data tests - single values"
-    (is (= "zero" (sut/digit->word 0 sut/british-english-numbers)))
-    (is (= "one"  (sut/digit->word 1 sut/british-english-numbers))))
+  #_(testing "Sample data tests - single values"
+      (is (= "zero" (sut/digit->word 0 sut/british-english-numbers)))
+      (is (= "one"  (sut/digit->word 1 sut/british-english-numbers))))
 
-  (testing "Sample data tests - double figure values"
-    (is (= "20" (sut/positional-number-string "21"))))
+  #_(testing "Sample data tests - double figure values"
+      (is (= "20" (sut/positional-number-string "21"))))
 
-  (testing "Generating sequences of numbers with positional number level"
-    (is (= ["20" "1"]   (sut/number-sequence 21)))
-    (is (= ["2000"]     (sut/number-sequence 2000)))
-    (is (= ["2000" "1"] (sut/number-sequence 2001))))
+  #_(testing "Generating sequences of numbers with positional number level"
+      (is (= ["20" "1"]   (sut/number-sequence 21)))
+      (is (= ["2000"]     (sut/number-sequence 2000)))
+      (is (= ["2000" "1"] (sut/number-sequence 2001))))
 
   ;; 2001 probably should be "2" "x000" "1"
 
 
-  (testing "Convert sequences of numbers to words"
+  #_(testing "Convert sequences of numbers to words"
+      (is (= ["zero"]
+             (sut/word-sequence british-english-dictionary ["0"])))
+      (is (= ["one"]
+             (sut/word-sequence british-english-dictionary ["1"])))
+      (is (= ["twenty"]
+             (sut/word-sequence british-english-dictionary ["20"])))
+      (is (= ["twenty" "one"]
+             (sut/word-sequence british-english-dictionary ["20" "1"])))
+      (is (= ["ninety" "nine"]
+             (sut/word-sequence british-english-dictionary ["90" "9"])))
+      (is (= ["one hundred"]
+             (sut/word-sequence british-english-dictionary ["100"])))
+      (is (= ["one hundred" "one"]
+             (sut/word-sequence british-english-dictionary ["100" "1"])))
+      (is (= ["nine hundred" "ninety" "nine"]
+             (sut/word-sequence british-english-dictionary ["900" "90" "9"])))
+      (is (= ["one thousand"]
+             (sut/word-sequence british-english-dictionary ["1000"])))
+      (is (= ["one thousand" "one"]
+             (sut/word-sequence british-english-dictionary ["1000" "1"])))
+      )
+
+  #_(testing "Parsing a number into a sequence of numbers"
+      (is (= ["0"],
+             (sut/parse-number ["0"])))
+      (is (= ["20" "1"],
+             (sut/parse-number ["21"])))
+      (is (= ["1" "x00"],
+             (sut/parse-number ["100"])))
+      (is (= ["10" "x000" "5" "x00" "40" "5"],
+             (sut/parse-number ["10545"]))))
+
+  (testing "Speaking the words as numbers"
     (is (= ["zero"]
-           (sut/word-sequence british-english-dictionary ["0"])))
+           (sut/speak-number-as-words british-english-dictionary [0])))
     (is (= ["one"]
-           (sut/word-sequence british-english-dictionary ["1"])))
-    (is (= ["twenty"]
-           (sut/word-sequence british-english-dictionary ["20"])))
-    (is (= ["twenty" "one"]
-           (sut/word-sequence british-english-dictionary ["20" "1"])))
-    (is (= ["ninety" "nine"]
-           (sut/word-sequence british-english-dictionary ["90" "9"])))
-    (is (= ["one hundred"]
-           (sut/word-sequence british-english-dictionary ["100"])))
-    (is (= ["one hundred" "one"]
-           (sut/word-sequence british-english-dictionary ["100" "1"])))
-    (is (= ["nine hundred" "ninety" "nine"]
-           (sut/word-sequence british-english-dictionary ["900" "90" "9"])))
-    (is (= ["one thousand"]
-           (sut/word-sequence british-english-dictionary ["1000"])))
-    (is (= ["one thousand" "one"]
-           (sut/word-sequence british-english-dictionary ["1000" "1"])))
-    )
-
-  (testing "Parsing a number into a sequence of numbers"
-    (is (= ["0"],
-           (sut/parse-number ["0"])))
-    (is (= ["20" "1"],
-           (sut/parse-number ["21"])))
-    (is (= ["1" "x00"],
-           (sut/parse-number ["100"])))
-    (is (= ["10" "x000" "5" "x00" "40" "5"],
-           (sut/parse-number ["10545"])))))
+           (sut/speak-number-as-words british-english-dictionary [1])))
+    (is (= ["twenty one"]
+           (sut/speak-number-as-words british-english-dictionary [21])))
+    (is (= ["one hundred and five"]
+           (sut/speak-number-as-words british-english-dictionary [105])))
+    (is (= ["one hundred and twenty three"]
+           (sut/speak-number-as-words british-english-dictionary [123])))
+    (is (= ["one thousand and five"]
+           (sut/speak-number-as-words british-english-dictionary [1005])))
+    (is (= ["one thousand and forty two"]
+           (sut/speak-number-as-words british-english-dictionary [1042])))
+    (is (= ["one thousand one hundred and five"]
+           (sut/speak-number-as-words british-english-dictionary [1105])))
+    (is (= ["fifty six million nine hundred and fourty five thousand seven hundred and eighty one"]
+           (sut/speak-number-as-words british-english-dictionary [56945781])))
+    (is (= ["nine hundred and ninety nine million nine hundred and ninety nine thousand
+             nine hundred and ninety nine"]
+           (sut/speak-number-as-words british-english-dictionary [999999999]))))
+  )
 
 
-
-;; Representative test cases
+;; Sample data test cases
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
 
 ;; |     Input | Output                                                                                                   |
 ;; |-----------|----------------------------------------------------------------------------------------------------------|
