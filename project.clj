e3c3ae6be01d15ebc97fe468d8dce8aea556d87a
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-09-02 21:04:21 +0100

    project configuration: clojure 1.10.1 and github repo url

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject word-conversion "0.1.0-SNAPSHOT"
+(defproject word-conversion "0.1.0"
   :description "Convert numberical representation of numbers to British English"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/practicalli/numbers-to-words"
   :license {:name "Creative Commons Attribution Share-Alike 4.0 International"
             :url  "https://creativecommons.org"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns word-conversion.core})
